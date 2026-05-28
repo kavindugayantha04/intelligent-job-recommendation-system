0f7f67fd4696d2c9e8d22c13bb2bbe429fba89b9
Shashika Dias <shashikadias09@gmail.com>
2026-05-28 15:38:29 +0530

    Update interviewApi.js

--- client/src/api/interviewApi.js
+++ client/src/api/interviewApi.js
@@ -6,7 +6,11 @@ export const getInterviewCandidates = () => axios.get("/getall/candidates");
 export const createInterviewApi = (data) =>
   axios.post("/interview/create", data);
 
+export const scheduleMyInterviewApi = (data) =>
+  axios.post("/interview/my/schedule", data);
+
 export const getAllInterviews = () => axios.get("/interview/all");
+export const getMyInterviewsApi = () => axios.get("/interview/my");
 
 export const getInterviewById = (id) => axios.get(`/interview/${id}`);
 
